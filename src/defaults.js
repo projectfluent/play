89d70b94f1bab0b445b1d02c1d67b36a29d40849
Staś Małolepszy <stas@mozilla.com>
2017-03-09 10:40:43 +0100

    Update to Fluent Syntax 0.3

--- src/defaults.js
+++ src/defaults.js
@@ -1,19 +1,21 @@
 export const translations = 
-`# Try editing the translations here!
+`// Try editing the translations here!
 
 hello-world = Hello, world!
 
-shared-photos = { $user_name } { $photo_count ->
-    [0] hasn't added any photos yet
-    [one] added a new photo
-   *[other] added { $photo_count } new photos
-}.
+shared-photos =
+    { $user_name } { $photo_count ->
+        [0] hasn't added any photos yet
+        [one] added a new photo
+       *[other] added { $photo_count } new photos
+    }.
 
-liked-comment = { $user_name } liked your comment on { $user_gender ->
-    [male] his
-    [female] her
-   *[other] their
-} post.`;
+liked-comment =
+    { $user_name } liked your comment on { $user_gender ->
+        [male] his
+        [female] her
+       *[other] their
+    } post.`;
 
 export const externals = {
   user_name: 'Anne',
