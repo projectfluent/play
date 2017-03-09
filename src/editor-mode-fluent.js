8e10a7824f7a9476f07258d85d7ff46b0469f25e
Staś Małolepszy <stas@mozilla.com>
2017-03-09 10:46:43 +0100

    Fluent ACE mode and theme

--- src/editor-mode-fluent.js
+++ src/editor-mode-fluent.js
@@ -1,104 +1,187 @@
 /* global ace */
 
-ace.define("ace/mode/fluent_highlight_rules",["require","exports","module","ace/lib/oop","ace/mode/text_highlight_rules"], function(acequire, exports, module) {
-var oop = acequire("../lib/oop");
-var TextHighlightRules = acequire("./text_highlight_rules").TextHighlightRules;
+ace.define(
+    "ace/mode/fluent_highlight_rules",
+    ["require", "exports", "module", "ace/lib/oop", "ace/mode/text_highlight_rules"],
+    highlighting
+);
 
-var FluentHighlightRules = function() {
+ace.define(
+    "ace/mode/fluent",
+    ["require", "exports", "module", "ace/lib/oop", "ace/mode/text", "ace/mode/fluent_highlight_rules"],
+    mode
+);
 
-    this.$rules = {
-        "start" : [
-            {
-                token : "comment",
-                regex : /#.*$/
-            },
-            {
-                token : "keyword",
-                regex : /\*?\[.*\]/,
-                push  : "value"
-            },
-            {
-                token : "entity.name.tag",
-                regex : /(.*=)\s*$/,
-            },
-            {
-                regex : /^\s*\|/,
-                token : "string",
-                push  : "value"
-            },
-            {
-                token : "entity.name.tag",
-                regex : /(.*=)/,
-                push  : "value"
-            },
-            {
-                defaultToken: "string"
-            }
-        ],
-        "placeable" : [
-            {
-                token : "entity.other",
-                regex : /^\s*\*?\[.*\]/,
-                push  : "value"
-            },
-            {
-                regex : /{/,
-                token : "variable.parameter",
-                push : "placeable"
-            },
-            {
-                regex : /}/,
-                token : "variable.parameter",
-                next : "pop"
-            },
-            {
-                defaultToken: "variable.parameter"
-            }
-        ],
-        "value" : [
-            {
-                regex : /^\s*\|/,
-                token : "string",
-            },
-            {
-                regex : /{/,
-                token : "variable.parameter",
-                push : "placeable"
-            },
-            {
-                regex : /^/,
-                token : "string",
-                next : "pop"
-            },
-            {
-                defaultToken: "string"
-            }
-        ],
+function highlighting(acequire, exports, module) {
+    const oop = acequire("../lib/oop");
+    const TextHighlightRules = acequire("./text_highlight_rules").TextHighlightRules;
+
+    const FluentHighlightRules = function() {
+
+        const _ = '\\s*';
+        const number = '[0-9]+(?:\\.[0-9]+)?';
+        const identifier = '[a-zA-Z_?-][a-zA-Z0-9_?-]*';
+        const word = '[^\\s{}\\[\\]\\\\]+';
+        const symbol = `${word}(?:[ \t]+${word})?`;
+
+        this.$rules = {
+            "start" : [
+                {
+                    token: "comment",
+                    regex: /^\/\/ .*$/
+                },
+                {
+                    token: "section",
+                    regex: /^\[\[/,
+                    push: "section"
+                },
+                {
+                    token: "message.identifier",
+                    regex: `^${identifier}${_}=`,
+                    push: "value"
+                },
+                {
+                    token : "message.identifier",
+                    regex: `^${identifier}${_}`,
+                },
+                {
+                    token: "message.attribute",
+                    regex: `^${_}\\.${identifier}${_}=`,
+                    push: "value"
+                },
+                {
+                    token: "message.tag",
+                    regex: `^${_}#${word}$`,
+                },
+                {
+                    defaultToken: "invalid"
+                }
+            ],
+            "section" : [
+                {
+                    token : "section",
+                    regex : /\]\]\s*$/,
+                    next: "pop"
+                },
+                {
+                    defaultToken: "section"
+                }
+            ],
+            "value" : [
+                {
+                    regex : /(?:^\s+)?{/,
+                    token : "placeable",
+                    push : "placeable"
+                },
+                {
+                    regex: /^\s+[^.#*[}\s]+/,
+                    token: "string"
+                },
+                {
+                    regex: /^\s*$/,
+                    token: "string"
+                },
+                {
+                    regex: /^/,
+                    next: "pop"
+                },
+                {
+                    defaultToken: "string"
+                }
+            ],
+            "placeable" : [
+                {
+                    regex : /^\S.*$/,
+                    token : "invalid",
+                    next : "pop"
+                },
+                {
+                    regex : `^(${_})(\\*?\\[${_})(${number})(${_}\\])`,
+                    token : ["text", "operator", "number", "operator"],
+                    push: "value"
+                },
+                {
+                    regex : `^(${_})(\\*?\\[${_})(${symbol})(${_}\\])`,
+                    token : ["text", "operator", "symbol", "operator"],
+                    push: "value"
+                },
+                {
+                    regex : /".*"/,
+                    token : "string.quoted"
+                },
+                {
+                    regex : /[A-Z_?-]+/,
+                    token : "function.name",
+                },
+                {
+                    regex : /\(/,
+                    token : "function.paren",
+                },
+                {
+                    regex : /\s*,\s*/,
+                    token : "function.comma",
+                },
+                {
+                    regex : `${identifier}\\s*:\\s*`,
+                    token : "function.argname",
+                },
+                {
+                    regex : /\)/,
+                    token : "function.paren"
+                },
+                {
+                    regex : number,
+                    token : "number"
+                },
+                {
+                    regex : `\\$${identifier}`,
+                    token : "variable"
+                },
+                {
+                    regex : `(${identifier})(\\[${symbol}\\])`,
+                    token : ["message.identifier", "symbol"]
+                },
+                {
+                    regex : `(${identifier})(\\.${identifier})`,
+                    token : ["message.identifier", "message.attribute"]
+                },
+                {
+                    regex : identifier,
+                    token : "message.identifier"
+                },
+                {
+                    regex : /\s*->\s*$/,
+                    token : "operator",
+                },
+                {
+                    regex : /}/,
+                    token : "placeable",
+                    next : "pop"
+                },
+            ]
+        };
+
+        this.normalizeRules();
+
     };
 
-    this.normalizeRules();
+    oop.inherits(FluentHighlightRules, TextHighlightRules);
+    exports.FluentHighlightRules = FluentHighlightRules;
+}
 
-};
+function mode(acequire, exports, module) {
+    const oop = acequire("../lib/oop");
+    const TextMode = acequire("./text").Mode;
+    const FluentHighlightRules = acequire("./fluent_highlight_rules").FluentHighlightRules;
 
-oop.inherits(FluentHighlightRules, TextHighlightRules);
+    const Mode = function() {
+        this.HighlightRules = FluentHighlightRules;
+    };
+    oop.inherits(Mode, TextMode);
 
-exports.FluentHighlightRules = FluentHighlightRules;
-});
+    (function() {
+        this.$id = "ace/mode/fluent";
+    }).call(Mode.prototype);
 
-ace.define("ace/mode/fluent",["require","exports","module","ace/lib/oop","ace/mode/text","ace/mode/fluent_highlight_rules"], function(acequire, exports, module) {
-
-var oop = acequire("../lib/oop");
-var TextMode = acequire("./text").Mode;
-var FluentHighlightRules = acequire("./fluent_highlight_rules").FluentHighlightRules;
-
-var Mode = function() {
-    this.HighlightRules = FluentHighlightRules;
-};
-oop.inherits(Mode, TextMode);
-
-(function() {
-    this.$id = "ace/mode/fluent";
-}).call(Mode.prototype);
-
-exports.Mode = Mode;
-});
+    exports.Mode = Mode;
+}
--- src/editor-theme-fluent.js
+++ src/editor-theme-fluent.js
@@ -1,138 +1,132 @@
 /* global ace */
 
-ace.define("ace/theme/fluent",["require","exports","module","ace/lib/dom"], function(acequire, exports, module) {
+ace.define(
+    "ace/theme/fluent",
+    ["require", "exports", "module", "ace/lib/dom"],
+    theme
+);
 
-exports.isDark = false;
-exports.cssClass = "ace-fluent";
-exports.cssText = `
+function theme(acequire, exports, module) {
+    exports.isDark = false;
+    exports.cssClass = "ace-fluent";
 
-.ace-fluent .ace_gutter {
-    background: #e8e8e8;
-    color: #AAA;
-}
 
-.ace-fluent  {
-    background: #f7f7f7;
-    color: #000;
-}
+    exports.cssText = `
+        .ace-fluent {
+            background-color: #f7f7f7;
+            color: #222;
+        }
 
-.ace-fluent .ace_name {
-    font-weight: bold;
-}
+        .ace-fluent .ace_cursor {
+            color: #222;
+        }
 
-.ace-fluent .ace_keyword {
-    font-weight: bold;
-}
+        .ace-fluent .ace_gutter {
+            background: #e8e8e8;
+            color: #aaa;
+        }
 
-.ace-fluent .ace_string {
-    color: #1a1aa6;
-}
+        .ace-fluent .ace_print-margin {
+            width: 1px;
+            background: #e8e8e8;
+        }
 
-.ace-fluent .ace_variable.ace_class {
-    color: teal;
-}
+        .ace-fluent .ace_invisible {
+            color: #bfbfbf
+        }
 
-.ace-fluent .ace_constant.ace_numeric {
-    color: #099;
-}
+        .ace-fluent .ace_indent-guide {
+            background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAE0lEQVQImWP4////f4bLly//BwAmVgd1/w11/gAAAABJRU5ErkJggg==") right repeat-y;
+        }
 
-.ace-fluent .ace_constant.ace_buildin {
-    color: #0086B3;
-}
+        .ace-fluent .ace_comment {
+            color: #999;
+            font-style: italic;
+        }
 
-.ace-fluent .ace_support.ace_function {
-    color: #0086B3;
-}
+        .ace-fluent .ace_section {
+            color: #666;
+        }
 
-.ace-fluent .ace_comment {
-    color: #998;
-    font-style: italic;
-}
+        .ace-fluent .ace_message {
+            font-weight: bold;
+        }
 
-.ace-fluent .ace_variable.ace_language  {
-    color: #0086B3;
-}
+        /*
+        .ace-fluent .ace_tag {
+            font-weight: normal;
+            color: #009926;
+        }
+        */
 
-.ace-fluent .ace_paren {
-    font-weight: bold;
-}
+        .ace-fluent .ace_string {
+            color: #1a1aa6;
+        }
 
-.ace-fluent .ace_boolean {
-    font-weight: bold;
-}
+        .ace-fluent .ace_number {
+            color: #08c;
+        }
 
-.ace-fluent .ace_string.ace_regexp {
-    color: #009926;
-    font-weight: normal;
-}
+        .ace-fluent .ace_symbol {
+            color: #222;
+        }
 
-.ace-fluent .ace_variable.ace_instance {
-    color: teal;
-}
+        .ace-fluent .ace_variable {
+            color: #930f80;
+        }
 
-.ace-fluent .ace_constant.ace_language {
-    font-weight: bold;
-}
+        .ace-fluent .ace_function {
+            color: #08c;
+        }
 
-.ace-fluent .ace_cursor {
-    color: black;
-}
+        .ace-fluent .ace_invalid {
+            background-color: rgba(255, 0, 0, 0.1);
+            color: red;
+        }
 
-.ace-fluent.ace_focus .ace_marker-layer .ace_active-line {
-    background: rgb(255, 255, 204);
-}
+        .ace-fluent.ace_focus .ace_marker-layer .ace_active-line {
+            background: rgb(255, 255, 204);
+        }
 
-.ace-fluent .ace_marker-layer .ace_active-line {
-    background: rgb(245, 245, 245);
-}
+        .ace-fluent .ace_marker-layer .ace_active-line {
+            background: rgb(245, 245, 245);
+        }
 
-.ace-fluent .ace_marker-layer .ace_selection {
-    background: rgb(181, 213, 255);
-}
+        .ace-fluent .ace_marker-layer .ace_selection {
+            background: rgb(181, 213, 255);
+        }
 
-.ace-fluent.ace_multiselect .ace_selection.ace_start {
-    box-shadow: 0 0 3px 0px white;
-}
+        .ace-fluent.ace_multiselect .ace_selection.ace_start {
+            box-shadow: 0 0 3px 0px white;
+        }
 
-.ace-fluent.ace_nobold .ace_line > span {
-    font-weight: normal !important;
-}
+        .ace-fluent.ace_nobold .ace_line > span {
+            font-weight: normal !important;
+        }
 
-.ace-fluent .ace_marker-layer .ace_step {
-    background: rgb(252, 255, 0);
-}
+        .ace-fluent .ace_marker-layer .ace_step {
+            background: rgb(252, 255, 0);
+        }
 
-.ace-fluent .ace_marker-layer .ace_stack {
-    background: rgb(164, 229, 101);
-}
+        .ace-fluent .ace_marker-layer .ace_stack {
+            background: rgb(164, 229, 101);
+        }
 
-.ace-fluent .ace_marker-layer .ace_bracket {
-    margin: -1px 0 0 -1px;
-    border: 1px solid rgb(192, 192, 192);
-}
+        .ace-fluent .ace_marker-layer .ace_bracket {
+            margin: -1px 0 0 -1px;
+            border: 1px solid rgb(192, 192, 192);
+        }
 
-.ace-fluent .ace_gutter-active-line {
-    background-color : rgba(0, 0, 0, 0.07);
-}
+        .ace-fluent .ace_gutter-active-line {
+            background-color : rgba(0, 0, 0, 0.07);
+        }
 
-.ace-fluent .ace_marker-layer .ace_selected-word {
-    background: rgb(250, 250, 255);
-    border: 1px solid rgb(200, 200, 250);
-}
+        .ace-fluent .ace_marker-layer .ace_selected-word {
+            background: rgb(250, 250, 255);
+            border: 1px solid rgb(200, 200, 250);
+        }
+    `;
 
-.ace-fluent .ace_invisible {
-    color: #BFBFBF;
-}
-
-.ace-fluent .ace_print-margin {
-    width: 1px;
-    background: #e8e8e8;
-}
-
-.ace-fluent .ace_indent-guide {
-    background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAE0lEQVQImWP4////f4bLly//BwAmVgd1/w11/gAAAABJRU5ErkJggg==") right repeat-y;
-}`;
-
-    var dom = acequire("../lib/dom");
+    const dom = acequire("../lib/dom");
     dom.importCssString(exports.cssText, exports.cssClass);
-});
+}
