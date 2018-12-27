fa1ed7684340193f42d581d4523561a9350d44ab
Staś Małolepszy <stas@mozilla.com>
2018-12-27 17:42:02 +0100

    Updates

--- src.7ed060e2.js
+++ src.7ed060e2.js
@@ -51724,7 +51724,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54982" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58021" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
