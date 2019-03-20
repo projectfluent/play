89cd79b2aeafa40944b89958606a0ab420ce82ef
Staś Małolepszy <stas@mozilla.com>
2019-03-20 11:43:32 +0100

    Hello, world

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use iron::prelude::*;
+use iron::status;
+
+fn main() {
+    Iron::new(|_: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello World!")))
+    }).http("localhost:3000").unwrap();
+}
