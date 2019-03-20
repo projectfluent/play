78b3567693e0deb2f68b8be8f7295ab718e35ace
Staś Małolepszy <stas@mozilla.com>
2019-03-20 12:29:30 +0100

    Read PORT

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use iron::prelude::*;
 use iron::status;
+use std::env;
 
 fn main() {
-    Iron::new(|_: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }).http("localhost:3000").unwrap();
+    let port = env::var("PORT")
+        .unwrap_or("8080".to_string())
+        .parse()
+        .expect("Unable to parse PORT into a number");
+    Iron::new(|_: &mut Request| Ok(Response::with((status::Ok, "Hello World!"))))
+        .http(("localhost", port))
+        .unwrap();
 }
