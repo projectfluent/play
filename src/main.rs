d803c75a5fb43099f110f82fc58b32e83dfd679c
Staś Małolepszy <stas@mozilla.com>
2019-03-21 11:47:47 +0100

    Use hubcaps to fetch gists

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,48 @@
-use iron::{status, Iron, IronResult, Request, Response};
+use hubcaps::{Credentials, Github};
+use iron::{status, Iron, Request, Response};
 use router::Router;
 use std::env;
-
-fn index(_: &mut Request) -> IronResult<Response> {
-    let resp = Response::with((status::Ok, "Hello, world!"));
-    Ok(resp)
-}
-
-fn gist_get(req: &mut Request) -> IronResult<Response> {
-    let params = req.extensions.get::<Router>().unwrap();
-    let name = params.find("id").unwrap();
-    let resp = Response::with((status::Ok, format!("Hello, {}!", name)));
-    Ok(resp)
-}
+use tokio::runtime::Runtime;
 
 fn main() {
     let port = env::var("PORT")
         .unwrap_or("8080".to_string())
         .parse()
         .expect("Unable to parse PORT into a number");
+    let token = env::var("GITHUB_API_TOKEN").expect("Missing GitHub API token");
 
-    let mut router: Router = Router::new();
-    router.get("/", index, "index");
-    router.get("/gist/:id", gist_get, "name");
+    let github = Github::new(
+        concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION")),
+        Credentials::Token(token),
+    );
+    let gists = github.gists();
+
+    let mut router = Router::new();
+    router.get(
+        "/",
+        |_req: &mut Request| {
+            let resp = Response::with((status::Ok, "Hello, world!"));
+            Ok(resp)
+        },
+        "index",
+    );
+    router.get(
+        "/gist/:id",
+        move |req: &mut Request| {
+            let params = req.extensions.get::<Router>().unwrap();
+            let id = params.find("id").unwrap();
+
+            let gist = Runtime::new()
+                .expect("Unable to create runtime")
+                .block_on(gists.get(id))
+                .expect("Unable to fetch gist");
+            println!("{:#?}", gist);
+
+            let resp = Response::with((status::Ok, format!("Gist id: {}", gist.id)));
+            Ok(resp)
+        },
+        "fetch",
+    );
 
     Iron::new(router).http(("0.0.0.0", port)).unwrap();
 }
