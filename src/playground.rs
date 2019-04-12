311546144031c7739d7d6dee64c9ea15c4afd0d7
Staś Małolepszy <stas@mozilla.com>
2019-04-12 19:50:27 +0200

    try_from when creating gists

--- src/playground.rs
+++ src/playground.rs
@@ -42,11 +42,14 @@ pub fn create(req: &mut Request) -> IronResult<Response> {
         .read_to_string(&mut payload)
         .expect("Failed to read request body");
     let playground = serde_json::from_str::<Playground>(&payload).unwrap();
-    let gist = Runtime::new()
-        .expect("Unable to create runtime")
-        .block_on(gists.create(&gists::GistOptions::from(playground)))
-        .expect("Unable to create gist");
-    match Playground::try_from(gist) {
+    let created = gists::GistOptions::try_from(playground).and_then(|options| {
+        let gist = Runtime::new()
+            .expect("Unable to create runtime")
+            .block_on(gists.create(&options))
+            .expect("Unable to create gist");
+        Playground::try_from(gist)
+    });
+    match created {
         Ok(playground) => json::respond(playground),
         Err(err) => json::error(err),
     }
@@ -61,7 +64,7 @@ fn try_file_content<'gist>(gist: &'gist gists::Gist, name: &str) -> Result<&'gis
         .ok_or(format!("Empty file in gist: {}", name))
 }
 
-fn try_json_value<'gist>(
+fn try_deserialize_json<'gist>(
     gist: &'gist gists::Gist,
     name: &str,
 ) -> Result<serde_json::value::Value, String> {
@@ -75,14 +78,19 @@ impl TryFrom<gists::Gist> for Playground {
         Ok(Playground {
             id: Some(gist.id.clone()),
             messages: try_file_content(&gist, "playground.ftl")?.clone(),
-            variables: try_json_value(&gist, "playground.json")?,
-            setup: try_json_value(&gist, "setup.json")?,
+            variables: try_deserialize_json(&gist, "playground.json")?,
+            setup: try_deserialize_json(&gist, "setup.json")?,
         })
     }
 }
 
-impl From<Playground> for gists::GistOptions {
-    fn from(playground: Playground) -> Self {
+fn try_serialize_json<'gist>(value: &'gist serde_json::value::Value) -> Result<String, String> {
+    serde_json::ser::to_string(value).or(Err("Error serializing playground".to_string()))
+}
+
+impl TryFrom<Playground> for gists::GistOptions {
+    type Error = String;
+    fn try_from(playground: Playground) -> Result<Self, Self::Error> {
         let mut files = HashMap::new();
         files.insert(
             "playground.ftl".to_string(),
@@ -95,20 +103,20 @@ impl From<Playground> for gists::GistOptions {
             "playground.json".to_string(),
             gists::Content {
                 filename: None,
-                content: serde_json::ser::to_string(&playground.variables).unwrap(),
+                content: try_serialize_json(&playground.variables)?,
             },
         );
         files.insert(
             "setup.json".to_string(),
             gists::Content {
                 filename: None,
-                content: serde_json::ser::to_string(&playground.setup).unwrap(),
+                content: try_serialize_json(&playground.setup)?,
             },
         );
-        gists::GistOptions {
+        Ok(gists::GistOptions {
             description: Some("A Fluent Playground snippet".to_string()),
             public: Some(true),
             files,
-        }
+        })
     }
 }
